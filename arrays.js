0bc9a369e50c8975e922bf47ddf803565c7cad76
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 18:41:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.lenth - 1);
+  return array.slice(0, array.length - 1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
