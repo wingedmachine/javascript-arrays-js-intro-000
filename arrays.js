ddd1c68e5c5da1fa287a9b123ef6983c822019df
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 18:34:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  //array.shift(element);
+  array.push(element);
   return array;
 }
