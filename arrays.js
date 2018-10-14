b3769f06262793cf5410db54e7410cfc01a94636
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 18:29:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,6 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array[0] = element;
+  array.unshift(element);
   return array;
 }
